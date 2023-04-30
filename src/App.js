767c224c125cbb4fab0ebe4c5587e92c55c814bd
NaveenjotKaur <130174280+NaveenjotKaur@users.noreply.github.com>
2023-04-30 15:22:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Body from './components/Body'
+import Header from './components/Header'
+
+import './styles/header.css'
+import './styles/body.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id='topic'>
+      <h1>Temperature Converter Website</h1>
+    </div>
+  )
+}
+
+export default Header
